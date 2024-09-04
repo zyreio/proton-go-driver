ad181d3dce8454cfab927fbe8a8374b8e5b0fa00
iNcizzle <incizzle@gmail.com>
2024-09-04 04:08:30 -0400

    bind parameters for async insert
    additions copied from https://github.com/ClickHouse/clickhouse-go/blob/main/conn_async_insert.go

--- conn_async_insert.go
+++ conn_async_insert.go
@@ -19,9 +19,11 @@ package proton
 
 import (
 	"context"
+
+	"github.com/timeplus-io/proton-go-driver/v2/lib/proto"
 )
 
-func (c *connect) asyncInsert(ctx context.Context, query string, wait bool) error {
+func (c *connect) asyncInsert(ctx context.Context, query string, wait bool, args ...any) error {
 	options := queryOptions(ctx)
 	{
 		options.settings["async_insert"] = 1
@@ -30,6 +32,16 @@ func (c *connect) asyncInsert(ctx context.Context, query string, wait bool) erro
 			options.settings["wait_for_async_insert"] = 1
 		}
 	}
+
+	if len(args) > 0 {
+		queryParamsProtocolSupport := c.revision >= proto.DBMS_MIN_PROTOCOL_VERSION_WITH_PARAMETERS
+		var err error
+		query, err = bindQueryOrAppendParameters(queryParamsProtocolSupport, &options, query, c.server.Timezone, args...)
+		if err != nil {
+			return err
+		}
+	}
+
 	if err := c.sendQuery(query, &options); err != nil {
 		return err
 	}
